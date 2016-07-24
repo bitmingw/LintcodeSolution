c0fdc8a4ea0c86a66bcaeb9410c6c88c93a5ffd2
Ming Wen <bitmingw@gmail.com>
2016-07-24 11:03:05 -0700

    complete 063

---
+++ 063SearchRotateSortArray2/main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+    /** 
+     * param A : an integer ratated sorted array and duplicates are allowed
+     * param target :  an integer to be search
+     * return : a boolean 
+     */
+public:
+    bool search(vector<int> &A, int target) {
+        // write your code here
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
